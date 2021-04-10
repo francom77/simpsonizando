373a583c89d5e3cfb522e6d7a7625f5afd274090
Franco Escobar <francomat77@gmail.com>
2021-04-10 17:45:21 -0300

    changed version to 1.0.1

--- addon.js
+++ addon.js
@@ -2,7 +2,7 @@ const { addonBuilder } = require("stremio-addon-sdk");
 
 const manifest = { 
     "id": "community.simpsonizando",
-    "version": "1.0.0",
+    "version": "1.0.1",
 
     "name": "Simpzonizando",
     "description": "Simpsons Latino",
