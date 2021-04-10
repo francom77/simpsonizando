cd3ad2f7854ecf110d3fdd228671e664db2e34ee
Franco Escobar <francomat77@gmail.com>
2021-04-10 17:44:22 -0300

    added chapter 2

--- addon.js
+++ addon.js
@@ -36,7 +36,8 @@ const manifest = {
 const dataset = {
     // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
     "tt0462538": { name: "The Simpsons Movie", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
-    "tt0096697:1:1": { name: "Simpsons Roasting on an Open Fire", type: "series", url: "http://lestraigocast.ddns.net:3000/Simpsons/s01/Los%20Simpsons%20-%20S01E01%20-%20Especial%20de%20Navidad.mp4"},
+    "tt0096697:1:1": { name: "S01E01", type: "series", url: "http://lestraigocast.ddns.net:3000/Simpsons/s01/Los%20Simpsons%20-%20S01E01%20-%20Especial%20de%20Navidad.mp4"},
+    "tt0096697:1:2": { name: "S01E02", type: "series", url: "http://lestraigocast.ddns.net:3000/Simpsons/s01/Los%20Simpsons%20-%20S01E02%20-%20Bart%20Es%20un%20Genio.mp4"},
 };
 
 const builder = new addonBuilder(manifest);
